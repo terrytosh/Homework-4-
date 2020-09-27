536bd2a210124eb719022e1a719818338e36c5e9
Terry Tosh <tmt103@txstate.edu>
2020-09-27 18:30:34 +0000

    test cases #2

--- hw4.cpp
+++ hw4.cpp
@@ -51,13 +51,11 @@ list_t appendHelper(list_t first, list_t second, list_t output_list) {
     output_list = list_make(list_first(first), output_list);
     return appendHelper(list_rest(first), second, output_list);
   }
-  if(list_isEmpty(second)) {
-    return output_list;
-  }
-  else {
+  else if(!list_isEmpty(second)) {
     output_list = list_make(list_first(second), output_list);
     return appendHelper(first, list_rest(second), output_list);
   }
+  return output_list;
 }
 
 int accumulate(list_t l, int (*fn)(int, int), int base) {
