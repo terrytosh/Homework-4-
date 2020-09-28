24441c1506b74357a8c5c885b1cd43d332fa3aad
Terry Tosh <tmt103@txstate.edu>
2020-09-28 04:24:54 +0000

    filter() test

--- hw4.cpp
+++ hw4.cpp
@@ -113,3 +113,18 @@ list_t filter_even(list_t list) {
   list_t output_list = list_make();
   return evenHelper(list, output_list);
 }
+
+list_t filterHelper(list_t list, bool (*fn)(int), list_t output_list) {
+  if(list_isEmpty(list)) {
+    return reverse(output_list);
+  }
+  if(fn(list_first(list))) {
+    output_list = list_make(list_first(list), output_list);
+  }
+  return filterHelper(list, fn, output_list);
+}
+
+list_t filter(list_t list, bool (*fn)(int)) {
+  list_t output_list = list_make();
+  return filterHelper(list, fn, output_list);
+}
